535ad5b262cfe3796c71bfe4bc2931b2c286d3b4
fluxhype <interact@adrianjo.se>
2016-12-17 23:51:09 -0500

    started asteroids exercise: ship is moving, asteroids are still broken.

--- asteroids/sketch.js
+++ asteroids/sketch.js
@@ -1,7 +1,36 @@
-function setup() {
+var ship;
+var asteroids = [];
 
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    ship = new Ship();
+    asteroids.push(new Asteroid());
 }
 
 function draw() {
+    background(0);
+    ship.render();
+    ship.turn();
+    ship.update();
+    ship.edges();
 
+    for (var i = 0; i < asteroids.length; i++) {
+        asteroids[i].render();
+    }
+
+}
+
+function keyReleased() {
+    ship.setRotation(0);
+    ship.boosting(false);
+}
+
+function keyPressed() {
+    if (keyCode == RIGHT_ARROW) {
+        ship.setRotation(0.1);
+    } else if (keyCode == LEFT_ARROW) {
+        ship.setRotation(-0.1)
+    } else if (keyCode == UP_ARROW) {
+        ship.boosting(true);
+    }
 }
