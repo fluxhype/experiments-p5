543a6d1aca032a8d87ae95f2a78f382054c2cd33
fluxhype <interact@adrianjo.se>
2016-12-20 23:13:22 -0500

    exercise complete

--- asteroids/ship.js
+++ asteroids/ship.js
@@ -28,7 +28,7 @@ function Ship() {
         push();
         translate(this.pos.x, this.pos.y);
         rotate(this.heading + PI / 2);
-        noFill();
+        fill(0);
         stroke(255);
         triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
         pop();
@@ -53,5 +53,14 @@ function Ship() {
 
     this.turn = function(angle) {
         this.heading += this.rotation;
+    }
+
+    this.hits = function(asteroid) {
+        var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
+        if (d < this.r + asteroid.r) {
+          return true;
+        } else {
+          return false;
+        }
     }
 }
