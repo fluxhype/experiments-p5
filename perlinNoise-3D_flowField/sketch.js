25377485ba03f054c99f82c622488d82cb18a512
fluxhype <interact@adrianjo.se>
2017-01-16 14:00:09 -0500

    complete flow field visualization

---
+++ perlinNoise-3D_flowField/sketch.js
@@ -0,0 +1,69 @@
+//fluxhype
+//Perlin Noise 3D - Flow Field
+//Exercise via Daniel Shiffman
+
+var inc = 0.1;
+var scl = 10;
+var cols, rows;
+
+var zoff = 0;
+
+var fr;
+
+var particles = [];
+
+var flowfield;
+
+function setup() {
+    colorMode(HSL);
+
+    createCanvas(600, 600);
+
+    cols = floor(width / scl);
+    rows = floor(height / scl);
+
+    fr = createP('');
+
+    flowfield = new Array(cols * rows);
+
+    for (var i = 0; i < 1500; i++) {
+        particles[i] = new Particle();
+    }
+    background(100);
+}
+
+function draw() {
+    var yoff = 0;
+
+    for (var y = 0; y < rows; y++) {
+        var xoff = 0;
+        for (var x = 0; x < cols; x++) {
+            var index = (x + y * cols);
+            var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+            var v = p5.Vector.fromAngle(angle);
+            v.setMag(1);
+            flowfield[index] = v;
+            xoff += inc;
+
+            stroke(0, .5);
+
+            // push();
+            // translate(x * scl, y * scl);
+            // rotate(v.heading());
+            // line(0, 0, scl, 0);
+            // pop();
+        }
+        yoff += inc;
+
+        zoff += 0.0003;
+    }
+
+    for (var i = 0; i < particles.length; i++) {
+        particles[i].follow(flowfield);
+        particles[i].update();
+        particles[i].edges();
+        particles[i].show();
+    }
+
+    fr.html(floor(frameRate()));
+}
